21b5bf5eed4b79d5e780705c933496b7b982c8a7
akhil bhumireddy <akhil.bhumireddy99@gmail.com>
2025-01-22 11:11:04 +0530

    updated code for deployment

--- src/components/SingleSelectDropdown.js
+++ src/components/SingleSelectDropdown.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
-import { cn } from "../lib/utils";
+import { cn } from "../libs/utils";
 
 export default function SingleSelectDropdown({
   options,
